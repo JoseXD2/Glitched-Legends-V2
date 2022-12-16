8eeee68073380146eb32d8fb0e528c5c0eb7edaf
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-12-16 15:09:27 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -35,7 +35,7 @@ class Main extends Sprite
 	var game = {
 		width: 1280, // WINDOW width
 		height: 720, // WINDOW height
-		initialState: Cache, // initial game state
+		initialState: TitleState, // initial game state
 		zoom: -1.0, // game state bounds
 		framerate: 60, // default framerate
 		skipSplash: true, // if the default flixel splash screen should be skipped
